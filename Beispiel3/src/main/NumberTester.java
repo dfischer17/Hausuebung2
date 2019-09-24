54599c25bfd443838fe519ca68a2656ad2441d4c
Daniel FISCHER <dfischer17@htl-grieskirchen.local>
2019-09-24 12:00:35 +0200

    example3 show file input

--- Beispiel3/src/main/NumberTester.java
+++ Beispiel3/src/main/NumberTester.java
@@ -5,17 +5,62 @@
  */
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author dfischer17
  */
 public class NumberTester {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static String fileName;
+    
+    public interface NumberTest {
+        boolean testNumber(int number);
     }
     
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+    
+    public static void main(String[] args) {
+        NumberTester n = new NumberTester("test.txt");
+        n.run();
+        
+    }
+        
+    public void run() {
+        try(final BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+            while (line != null) {
+                line = br.readLine();
+                System.out.println(line);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found!");
+        } catch (IOException ex) {
+            System.out.println("IO Exception!");
+        }
+    }
+    
+    public void setOddEvenTester(NumberTest oddTester) {
+        
+    }
+    
+    public void setPrimeTester(NumberTest primeTester) {
+        
+    }
+    
+    public void setPalindromTester(NumberTest palindromTester) {
+      
+    }
+    
+    public void testFile() {
+        
+    }
 }
