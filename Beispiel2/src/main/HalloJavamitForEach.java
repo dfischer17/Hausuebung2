c755c49eaacc34ef078780d5951d2c8c23f78e15
Daniel FISCHER <dfischer17@htl-grieskirchen.local>
2019-09-24 11:29:23 +0200

    added dobble dot for-each loop

--- Beispiel2/src/main/HalloJavamitForEach.java
+++ Beispiel2/src/main/HalloJavamitForEach.java
@@ -20,11 +20,19 @@ public class HalloJavamitForEach {
     private static List<String> myList = new ArrayList<>();
     
     public static void main(String[] args) {
+        System.out.println("for-each List Interface");
         myList.add("Apfel");
         myList.add("Birne");
-        myList.add("Banane");
-        
+        myList.add("Banane");                     
         myList.forEach((str) -> System.out.println(str));
+        
+        System.out.println("for-each vereinfachte for-Schleife");
+        for(String string: myList) {
+            System.out.println(string);
+        }
+        
+        System.out.println("for-each zwei Doppelpunkte");
+        myList.forEach(System.out::println);
     }
     
 }
