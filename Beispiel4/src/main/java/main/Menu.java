3ee566fdc6a56010ced9c688d689a7b16f4cfe5b
dfischer17 <dfischer17htl@outlook.com>
2019-09-26 23:59:47 +0200

    example4 implemented Menu class

---
+++ Beispiel4/src/main/java/main/Menu.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel Fischer
+ */
+public class Menu {
+    private static Scanner input = new Scanner(System.in);
+    
+    public static void chooseCalcMenu() {
+        System.out.println("1- Relational calculator");
+        System.out.println("Vector calculator");
+        System.out.println("Complex calculator");
+        System.out.println("Exit program");
+    }
+    
+    public static double[] fetchNumbers() {
+        double[] numberValues = new double[4];
+        
+        System.out.println("Enter number x a>");
+        numberValues[0] = input.nextDouble();
+        
+        System.out.println("Enter number x b>");
+        numberValues[1] = input.nextDouble();
+        
+        System.out.println("Enter number y a>");
+        numberValues[2] = input.nextDouble();
+        
+        System.out.println("Enter number y v>");
+        numberValues[3] = input.nextDouble();
+        
+        return numberValues;
+    }
+    
+    public static void chooseOperationMenu() {
+        System.out.println("1 - add");
+        System.out.println("2 - subtract");
+        System.out.println("3 - multiply");
+        System.out.println("4 - divide");
+        System.out.println("5 - enter numbers again");
+    }
+}
+
--- Beispiel4/src/main/java/main/VectorCalculator.java
+++ Beispiel4/src/main/java/main/VectorCalculator.java
@@ -9,6 +9,62 @@ package main;
  *
  * @author Daniel Fischer
  */
-public class VectorCalculator {
+public class VectorCalculator extends AbstractCalculator {
+
+    public VectorCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+                
+    public static void main(String[] args) {
+        VectorCalculator v = new VectorCalculator(addLambda, subtractLambda, multiplyLambda, divideLambda);
+        System.out.println(v.divide(new Number(5, 1), new Number(5, 6)).toString());
+    }
     
+    static CalculationOperation addLambda = (n, k) -> {
+        double top = n.getA()  + k.getA();
+        double bottom = n.getB() + k.getB();
+        
+        return new Number(top, bottom);
+    };
+    
+    static CalculationOperation subtractLambda = (n, k) -> {
+        double top = n.getA()  - k.getA();
+        double bottom = n.getB() - k.getB();
+        
+        return new Number(top, bottom);
+    };
+    
+    static CalculationOperation multiplyLambda = (n, k) -> {
+        // Nur in 3D moeglich!
+        System.err.println("Nur in 3D moeglich!");
+        return null;
+    };
+    
+    static CalculationOperation divideLambda = (n, k) -> {
+       // Skalarprodukt
+       double top = n.getA() / n.getB() * k.getA();
+       double bottom = n.getA() / n.getB() * k.getB();
+       
+       return new Number(top, bottom);
+    };
+
+    @Override
+    public Number add(Number a, Number b) {
+        return addLambda.calc(a, b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return subtractLambda.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return multiplyLambda.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return divideLambda.calc(a, b);
+    }
 }
